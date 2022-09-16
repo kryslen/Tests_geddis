eb1f1745f0bb0e31bd437c9a3a88b20645c77bf3
Krystyna is a GOD <113180144+kryslen@users.noreply.github.com>
2022-09-16 18:51:36 +0300

    Update Multiplication sheet test.py

--- Multiplication sheet test.py
+++ Multiplication sheet test.py
@@ -1,8 +1,8 @@
 def main():
     a,b,c,d=int(input()), int(input()), int(input()), int(input())
     print ('')
-    for i in range (c, d+1):
-        print('\t', i, end='') # adding row
+    for y in range (c, d+1):
+        print('\t', y, end='') # adding row
     for i in range (a, b+1): #adding column
         print ('\n', i, end='')
         for j in range (c, d+1): #multiple
